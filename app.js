a7ce5a980826f14f62645a15eba21d68d09b4cfb
Another <kaua.rodriguesfaria607@gmail.com>
2025-03-20 21:31:55 -0300

    Update app.js

--- app.js
+++ app.js
@@ -5,7 +5,13 @@
 //subtitulo.innerHTML = 'coloque um numero entre 1 a 10';
 
 exibirTextoNaTela('h1', 'jogo de adivinhar');
-exibirTextoNaTela('p', 'escolha um numero entre 1 a 10');
+exibirTextoNaTela('p', 'escolha um numero entre 1 a 100');
+
+function gerarNumeroAleatorio() {
+    return parseInt(Math.random() * 100) + 1;
+} 
+
+let numeroSecreto = gerarNumeroAleatorio
 
 function exibirTextoNaTela(tag, texto) {
     let campo = document.querySelector(tag, texto);
