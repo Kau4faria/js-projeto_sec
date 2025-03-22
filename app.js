9349de39be28d7ea5d3b14cde8cbd1b539e762cb
Another <kaua.rodriguesfaria607@gmail.com>
2025-03-21 21:44:27 -0300

    jogo da ativinhaçao 2.0

--- app.js
+++ app.js
@@ -3,16 +3,16 @@
 
 //let subtitulo = document.querySelector('p');
 //subtitulo.innerHTML = 'coloque um numero entre 1 a 10';
-
+let numeroChute = 5;
 exibirTextoNaTela('h1', 'jogo de adivinhar');
-exibirTextoNaTela('p', 'escolha um numero entre 1 a 100');
+exibirTextoNaTela('p', 'escolha um numero entre 1 a 10');
 
 function gerarNumeroAleatorio() {
-    return parseInt(Math.random() * 100) + 1;
+    return parseInt(Math.random() * 10) + 1;
 } 
 
-let numeroSecreto = gerarNumeroAleatorio
-
+let numeroSecreto = gerarNumeroAleatorio();
+console.log(numeroSecreto)
 
 function exibirTextoNaTela(tag, texto) {
     let campo = document.querySelector(tag, texto);
@@ -20,6 +20,21 @@ function exibirTextoNaTela(tag, texto) {
 }
 
 function verificarChute() {
-    let chute = parseInt(document.querySelector('input')).value
-    console.log(chute === numeroSecreto);
+    if(numeroChute > 0){
+        let chute = parseInt(document.querySelector('input').value);
+        numeroChute--;
+        let palavraChute = numeroChute === 1 ? 'chute' : 'chutes'
+        if(chute === numeroSecreto){
+          exibirTextoNaTela('p', 'voce acertou');
+        } else {
+            if(numeroChute === 0){
+                exibirTextoNaTela('p', `voce errou, voce nao tem mais tentativas`);
+                console.log(numeroChute)
+            } else if(chute < numeroSecreto){
+                exibirTextoNaTela('p', `voce errou, o numero é maior, voce tem mais ${numeroChute} ${palavraChute}`)
+            } else{
+                exibirTextoNaTela('p', `voce errou, o numero é menor, voce tem mais ${numeroChute} ${palavraChute}`)
+            }
+        }
+    }
 }
